af20417ab1e747a07bf44f6c610505b3d7a9b845
AshishVer <ashish625verma@gmail.com>
2017-03-23 00:30:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/t', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'thoughts.html'));
 });
 var cunter=0;
